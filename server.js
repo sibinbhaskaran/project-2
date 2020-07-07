8a1c5f63a2ac820a469d94380d17506bbb3b58ab
sibin bhaskaran <sibinbhaskaran@sibins-MacBook-Pro.local>
2020-07-07 13:36:09 -0500

    basic express working

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 3000;
 //Database
 //___________________
 // How to connect to the database either via heroku or locally
-const MONGODB_URI = process.env.MONGODB_URI
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/storedb'
 
 // Connect to Mongo
 mongoose.connect(MONGODB_URI ,  { useNewUrlParser: true});
